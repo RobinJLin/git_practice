f5426fda34ab07981193abd01efe322eca78446e
RobinJLin <dreamhopeandfaith@gmail.com>
2024-04-03 19:54:22 +0800

    add project

---
+++ project.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+import os
+import sys
+import json
+
+BASEPATH = os.path.dirname(os.path.abspath(__file__))
+
+
+if __name__ == "__main__":
+
+    print("第一次學 Git")
